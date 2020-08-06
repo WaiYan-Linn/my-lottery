0333a0e865e2b55f04cbe845454163f7a9a3534d
WaiYanLinnn <waiyanlin2015015@gmail.com>
2020-08-07 00:27:01 +0630

    first-commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCpZZAMJrYH6XXg7reWsVmvZa2GLVDaSHU",
+    authDomain: "lottery-4444.firebaseapp.com",
+    databaseURL: "https://lottery-4444.firebaseio.com",
+    projectId: "lottery-4444",
+    storageBucket: "lottery-4444.appspot.com",
+    messagingSenderId: "788291344639",
+    appId: "1:788291344639:web:221d610fd35ad7cb2014ca",
+    measurementId: "G-85R3J0Z1DZ"
+  }
 };
